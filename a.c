c53f2850f7f6e38578bb64d02cb80fa5e661f84c
zhaohuizeng <zhaohui.zeng@auperastor.com>
2019-07-19 10:30:28 +0800

    1.modify a.c add printf;2.modify b.c add printf

--- a.c
+++ a.c
@@ -2,6 +2,8 @@ iint  man()
 {
  printf("Hello,I am a a.c");
  printf("Hello Derick");
+
+ pritntf("test commit");
  return 1;
 
 }
--- b.c
+++ b.c
@@ -1,7 +1,7 @@
 int button()
 {
   int a=1,b=2;
-
+  print("test b.c commit");
   return (a+b);
 
 }
