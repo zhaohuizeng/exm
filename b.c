e03cd7125625318e09c26c9bc1c85b109d329bc0
zhaohuizeng <zhaohui.zeng@aueprastor.com>
2019-08-10 12:27:58 +0800

    modify1 b.c

--- b.c
+++ b.c
@@ -1,13 +1,5 @@
 int button()
 {
-  int a=1,b=2;
-  print("test b.c commit");
-  pritf
-  return (a+b);
-  return a;
-  return c;
-<<<<<<< HEAD
-  return (a+b+c);
-=======
->>>>>>> 6183e60b7c9c0929c7830094fb444f5f21c69c6d
+modify 1 b.c
+
 }
